f049f3be29d59efd14a250538cf554413e13e9ec
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2025-01-08 09:37:53 +0100

    Update index.tsx to match the blogpost

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import './index.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-const container = document.getElementById('app-root')
-const root = createRoot(container!)
+const container = document.getElementById('app-root')!
+const root = createRoot(container)
 root.render(<h1>Hello React!</h1>)
